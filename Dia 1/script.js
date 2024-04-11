bfd96974e168f9be15418086577b49eb8b3e7f80
Jorge Mozo <jorgy29081@gmail.com>
2024-04-11 09:02:25 -0500

    feat: :sparkles: EJERCICIO- CONVERSION DE CELCIUS A FAHRENHEIGT

--- Dia 1/script.js
+++ Dia 1/script.js
@@ -1,3 +1,23 @@
 a= 45
 b=62 
 console.log (a+b)
+
+//######################################//
+
+//funcion para pasar de celcius a fahrenheit//
+
+
+// F = 32 + (9 * C /5) //
+
+function conversion(n) {
+    return ((n * 9 / 5)+ 32)
+}
+
+const n = 20 ;
+
+console.log(conversion(n)) 
+
+
+
+
+
