da8610e9551f7a34a75c9b4328e3716201a329fa
Jorge Mozo <jorgy29081@gmail.com>
2024-04-12 08:36:00 -0500

    feat: :sparkles: Proyecto CRUD

---
+++ Dia 2/JS/CRUD.js
@@ -0,0 +1,2 @@
+import datos from  "../Data/Datos.json" assert {type:"json"};
+import { campers } from "./Clases.js";
---
+++ Dia 2/JS/Clases.js
@@ -0,0 +1,14 @@
+export class campers {
+    constructor(id, nombres, apellidos, direccion, acudiente, celular, telefono, estado, riesgo ) {
+        this.id=id;
+        this.id=id;
+        this.nombres=nombres;
+        this.apellidos=apellidos;
+        this.direccion=direccion;
+        this.acudiente=acudiente;
+        this.celular=celular;
+        this.telefono=telefono;
+        this.estado=estado;
+        this.riesgo=estado;
+    }
+}
