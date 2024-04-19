4f739595ecd0bb7caa23afaba9a0949439ae026b
Jorge Mozo <jorgy29081@gmail.com>
2024-04-19 07:12:23 -0500

    feat: :sparkles: Agregué ejercicicios faltantes de Objetos

--- Dia 4/Objetos.js
+++ Dia 4/Objetos.js
@@ -5,3 +5,43 @@ const persona = {
 }
 
 console.log(persona);
+
+const carro = {
+    Marca: 'Renault' ,
+    Modelo: 'Logan-2019' ,
+    Color: 'Gris' ,
+    Precio: '$21.000.000.00 COP'
+
+}
+
+console.log(carro);
+
+
+const libro = {
+    Género: 'Tragedia' ,
+    Nombre: 'Romeo y Julieta' ,
+    Año: '1597' ,
+    Autor: 'William Shaskespeare'
+}
+
+console.log(libro);
+
+
+let numerito = new Number(19);
+
+console.log(numerito.valueOf());
+
+
+function multiplicación(n) {
+    return (n * 4)
+}
+
+const n = 6
+
+console.log (multiplicación(n));
+
+
+
+
+
+
