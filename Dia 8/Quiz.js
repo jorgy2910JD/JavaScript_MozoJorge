4ba4a52c10c0c72de154586469e3d9b7838d8d02
Jorge Mozo <jorgy29081@gmail.com>
2024-04-23 10:04:01 -0500

    feat: :bug: Correccion de archivo js y implementacion del css

--- Dia 8/Quiz.js
+++ Dia 8/Quiz.js
@@ -1,7 +1,11 @@
+function Swapi ();
+
 fetch ("https://swapi.py4e.com/api/people/1/")
 
-.then(res=> console.log(res)) 
-
+.then(res=> res.json()) 
+.then(response=> {
+    console.log(response)
+})
 
 
 
