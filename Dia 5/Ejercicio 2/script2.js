249539ea3c3d83b18533f519a1552abd31708de7
Jorge Mozo <jorgy29081@gmail.com>
2024-04-23 19:44:52 -0500

    feat: :sparkles: Ejercicio 2 hecho

--- Dia 5/Ejercicio 2/script2.js
+++ Dia 5/Ejercicio 2/script2.js
@@ -1,3 +1,31 @@
-class rectangule {
-    
+//Definir la clase rectangulo
+class rectangulo {
+    constructor(ancho, altura) {
+        this.ancho= ancho;
+        this.altura= altura;
+    }
+//Método para calcular el area y perimetro
+    Area() {
+        return this.ancho * this.altura;
+    }
+    Perimetro() {
+        return (this.ancho + this.altura) * 2 ;
+
+    }
+
 }
+//Istancia para el clase rectangulo 
+let Nuevorectangulo = rectangulo(14, 7);
+
+//Calculo del área y perímetro del rectangulo
+let area = Nuevorectangulo.Area();
+let perimetro = Nuevorectangulo.Perimetro();
+
+//Resultado mostrado en la consola 
+console.log ("El Area del rectangulo pedido es:" + area);
+console.log ("El Perimetro del rectangulo pedido es:" + perimetro);
+
+
+
+
+
