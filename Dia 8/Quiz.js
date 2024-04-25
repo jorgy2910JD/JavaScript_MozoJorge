542c28eacbdfa191f2803ab5beb9a9e3eb743b3c
Jorge Mozo <jorgy29081@gmail.com>
2024-04-24 21:43:59 -0500

    feat: :bug: correccion del Arch JS y tabla de datos

--- Dia 8/Quiz.js
+++ Dia 8/Quiz.js
@@ -1,12 +1,54 @@
-function Swapi ();
+function fecth() {
+    let xhr = new XMLHttpRequest();
+    let url = 'https://swapi.py4e.com/api/people/1/' ;
 
-fetch ("https://swapi.py4e.com/api/people/1/")
+    xhr.open('GET',url,true)
+    xhr.onreadystatechange=function(){
+        if(this.readyState ===4 && this.status===200){
+            let response =JSON.parse(this.responseText);
+            console.log(response);
+        }
+        else{
+            console.log("ERROR!")
+        }
+    }
+    xhr.send();
+    
 
-.then(res=> res.json()) 
-.then(response=> {
-    console.log(response)
-})
+}
+function displayStar (Data) {
+    let starInfo = document.getElementById('Datos');
+    if (Data.response==="error") {
+        starInfo.innerHTML= '<p> Error!!! </p>'
 
+    }else {
+        starInfo.innerHTML=`
+        <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Height</th>
+            <th scope="col">Mass</th>
+            <th scope="col">Hair Color</th>
+            <th scope="col">Eye Color</th>
+            <th scope="col">Birth</th>
+            <th scope="col">Gender</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </table> 
+      `
+    }
+}
+fecth();
 
 
 
